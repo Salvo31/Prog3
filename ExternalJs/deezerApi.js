4041dc1646b1be52d80f16e347227838ef00137c
Salvatore <sasix280@gmail.com>
2022-05-09 19:15:07 +0200

    Fixato DEEZER Vedere se riesco a portarlo su file externo

--- ExternalJs/deezerApi.js
+++ ExternalJs/deezerApi.js
@@ -1,10 +1,11 @@
 	DZ.init({
 		appId  : '539622',
-		channelUrl : '../deezerChannel.html',
+		channelUrl : 'http://developers.deezer.com/examples/channel.php',
 		player: {
 			container: 'player',
 			width : 800,
 			height : 300,
+			format : 'square',
 			onload : function(){}
 		}
 	});
